29747cf81e65673f6f4b86d1bc7db529e818bfda
Howaida Ahmed <62309638+howidaahmed@users.noreply.github.com>
2023-07-02 22:41:49 +0300

    added splash ,screen1

---
+++ lib/ScreenOne.dart
@@ -0,0 +1,176 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:untitled2/ui/sign_in.dart';
+class ScreenOne extends StatelessWidget {
+  static const String routeName = 'ScreenOne';
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+          body: SingleChildScrollView(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: [
+
+                        Image(
+                          image: AssetImage("asstes/images/appbBar.png"),
+                          width: double.infinity,
+                          height: MediaQuery
+                              .of(context)
+                              .size
+                              .height * .60,
+                          fit: BoxFit.fill,
+                        ),
+
+                        const Positioned(
+                          left: 18,
+                          top: 130,
+                          child: Text(
+                            'Welcome to Baheya',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height:300 ,),
+                        const Positioned(
+                          left: 15,
+                          top: 340,
+                          child: Text(
+                            '''
+Baheya, standing back to 
+back with all Egyptian women
+                            ''',
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 100,),
+                        const Positioned(
+                          left: 15,
+                          top: 420,
+                          child: Text(
+                            '''
+ Baheya’s story has started when Mrs. Baheya Wahby, a 
+ lady from a prominent Egyptian family, was diagnosed 
+ with cancer back in 1990s. Because she went through
+ the same experience she felt for the fighters and 
+ especially the less privileged women.
+                            ''',
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                        )
+
+                      ],
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text('Or login with',style: TextStyle(fontSize: 18),),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            googleConnect();
+                          },
+                          child: Container(
+                              width: 35,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child:
+                              Image(image: AssetImage('asstes/images/google.jpg'))),
+                        ),
+                        Text('Google'),
+                        SizedBox(
+                          width: 40,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            //facebookConnect();
+                          },
+                          child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: const Image(
+                                  image: AssetImage('asstes/images/facebook.jpg'))),
+                        ),
+                        const Text('Facebook')
+                      ],
+                    ),
+                    SizedBox(height: 70,),
+                    Row(
+                    mainAxisAlignment: MainAxisAlignment.center
+                    ,children: [
+                      Text('Already have an account?'),
+                      TextButton(onPressed: (){
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => Sign_IN()),
+                        );
+                      }, child: Text('Signin',style: TextStyle(
+                        color: Color(0xffF79489)
+                      ),))
+                    ],),
+
+
+
+                  ])
+          )
+
+      );
+  }
+
+
+  void googleConnect() async {
+    Future<UserCredential> signInWithGoogle() async {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    }
+    // }
+    //
+    // void facebookConnect() {
+    //
+    //   Future<UserCredential> signInWithFacebook() async {
+    //     // Trigger the sign-in flow
+    //     final LoginResult loginResult = await FacebookAuth.instance.login();
+    //
+    //     // Create a credential from the access token
+    //     final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken.token);
+    //
+    //     // Once signed in, return the UserCredential
+    //     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+    //   }
+  }
+
+
+
+}
+
